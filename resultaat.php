76a6c7732e93cded89b1f02c932eb80e215df95a
ProsperityGH <cskins22@gmail.com>
2022-03-14 19:17:36 +0100

    php

--- resultaat.php
+++ resultaat.php
@@ -1,7 +1,7 @@
 <?php
-// $vraag1 = $_GET_['vraag1[0]'];
-// $vraag2 = $_GET_['vraag2'];
-// $vraag3 = $_GET_['vraag3'];
-// $vraag4 = $_GET_['vraag4'];
-// $vraag5 = $_GET_['vraag5'];
+$vraag1 = $_GET['vraag[0]'];
+$vraag2 = $_GET['vraag[1]'];
+$vraag3 = $_GET['vraag[2]'];
+$vraag4 = $_GET['vraag[3]'];
+$vraag5 = $_GET['vraag[4]'];
 ?>
