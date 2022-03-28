cf7d6e87dc6fb4aa3417c2f3b97adeca1e2d6bdd
ProsperityGH <cskins22@gmail.com>
2022-03-28 12:37:09 +0200

    Verbetering

--- resultaat.php
+++ resultaat.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/resultaat.css">
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
@@ -41,35 +41,35 @@ if(isset($_GET['submit'])){
 
         $vraag1=$_GET['vraag1'];
         echo "Het antwoord op vraag 1 is: $vraag1";
-        echo "<p style='margin-top: 15px;'>";
+        echo "<p style='margin-top: 10px;'>";
         echo "<br>";
     }
     if(!empty($_GET['vraag2'])) {
 
         $vraag2=$_GET['vraag2'];
         echo "Het antwoord op vraag 2 is: $vraag2";
-        echo "<p style='margin-top: 15px;'>";
+        echo "<p style='margin-top: 10px;'>";
         echo "<br>";
     }
     if(!empty($_GET['vraag3'])) {
 
         $vraag3=$_GET['vraag3'];
         echo "Het antwoord op vraag 3 is: $vraag3";
-        echo "<p style='margin-top: 15px;'>";
+        echo "<p style='margin-top: 10px;'>";
         echo "<br>";
     }
     if(!empty($_GET['vraag4'])) {
 
         $vraag4=$_GET['vraag4'];
         echo "Het antwoord op vraag 4 is: $vraag4";
-        echo "<p style='margin-top: 15px;'>";
+        echo "<p style='margin-top: 10px;'>";
         echo "<br>";
     }
     if(!empty($_GET['vraag5'])) {
 
         $vraag5=$_GET['vraag5'];
         echo "Het antwoord op vraag 5 is: $vraag5";
-        echo "<p style='margin-top: 15px;'>";
+        echo "<p style='margin-top: 10px;'>";
         echo "<br>";
     }
 }
